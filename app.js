04a0f8cc0d0314401df81d95122c11eecae7521b
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-05-09 00:57:05 -0400

    craeting a server file

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const path = require('path');
+const env=require('dotenv');
+const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+  // All other routes respond with the index.html file
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+
+app.listen( process.env.PORT || 5000 ,()=>{
+    console.log("Server is running ...")
+})
+
